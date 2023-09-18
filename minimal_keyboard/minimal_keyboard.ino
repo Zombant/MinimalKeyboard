e2dc7e52f123efae60de61dc8d1a7c6275f7290d
Zombant <anthony.piccinone@gmail.com>
2023-09-17 20:59:19 -0400

    Removed enter key

--- minimal_keyboard/minimal_keyboard.ino
+++ minimal_keyboard/minimal_keyboard.ino
@@ -4,21 +4,19 @@
 #define ONE_PIN 7
 #define ENTER_PIN 16
 
-int prevZeroPressed, prevOnePressed, prevEnterPressed = HIGH;
+int prevZeroPressed, prevOnePressed = HIGH;
 
 void setup() {
   Serial.begin(9600);
   Keyboard.begin();
   pinMode(ZERO_PIN, INPUT_PULLUP);
   pinMode(ONE_PIN, INPUT_PULLUP);
-  pinMode(ENTER_PIN, INPUT_PULLUP);
 }
 
 // TODO: FSM
 void loop() {
   int zeroPressed = digitalRead(ZERO_PIN);
   int onePressed = digitalRead(ONE_PIN);
-  int enterPressed = digitalRead(ENTER_PIN);
   if(zeroPressed == LOW && prevZeroPressed == HIGH) {
     Keyboard.press('0');
     delay(50);
@@ -35,15 +33,6 @@ void loop() {
     Keyboard.release('1');
     delay(50);
   }
-  if(enterPressed == LOW && prevEnterPressed == HIGH) {
-    Keyboard.press(10);
-    delay(50);
-  }
-  if(enterPressed == HIGH && prevEnterPressed == LOW) {
-    Keyboard.release(10);
-    delay(50);
-  }
   prevZeroPressed = zeroPressed;
   prevOnePressed = onePressed;
-  prevEnterPressed = enterPressed;
 }
